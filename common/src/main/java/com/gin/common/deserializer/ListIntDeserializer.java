e0a8d42fca312a6776c12fb54ec68e8e6d25ea21
ginstone <hjg719@139.com>
2023-04-13 15:15:36 +0800

    反序列化类

---
+++ common/src/main/java/com/gin/common/deserializer/ListIntDeserializer.java
@@ -0,0 +1,24 @@
+package com.gin.common.deserializer;
+
+import com.fasterxml.jackson.core.JacksonException;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * 整数列表反序列化
+ * @author : ginstone
+ * @version : v1.0.0
+ * @since : 2023/4/13 15:12
+ */
+public class ListIntDeserializer extends JsonDeserializer<List<Integer>> {
+    @Override
+    public List<Integer> deserialize(JsonParser p, DeserializationContext deserializationContext) throws IOException, JacksonException {
+        return Arrays.stream(p.getValueAsString().split(",")).map(Integer::parseInt).collect(Collectors.toList());
+    }
+}
