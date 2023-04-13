2c99ba8816b394ea0becce86479de5a4d466fce0
ginstone <hjg719@139.com>
2023-04-13 15:42:35 +0800

    字符串反序列化

--- common/src/main/java/com/gin/common/deserializer/ListIntDeserializer.java
+++ common/src/main/java/com/gin/common/deserializer/ListIntDeserializer.java
@@ -4,8 +4,10 @@ import com.fasterxml.jackson.core.JacksonException;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonDeserializer;
+import org.springframework.util.ObjectUtils;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -19,6 +21,12 @@ import java.util.stream.Collectors;
 public class ListIntDeserializer extends JsonDeserializer<List<Integer>> {
     @Override
     public List<Integer> deserialize(JsonParser p, DeserializationContext deserializationContext) throws IOException, JacksonException {
-        return Arrays.stream(p.getValueAsString().split(",")).map(Integer::parseInt).collect(Collectors.toList());
+        final String value = p.getValueAsString();
+        if (ObjectUtils.isEmpty(value)) {
+            return new ArrayList<>();
+        }
+        return Arrays.stream(value.split(","))
+                .filter(i->!ObjectUtils.isEmpty(i))
+                .map(Integer::parseInt).collect(Collectors.toList());
     }
 }
---
+++ common/src/main/java/com/gin/common/deserializer/ListStringDeserializer.java
@@ -0,0 +1,23 @@
+package com.gin.common.deserializer;
+
+import com.fasterxml.jackson.core.JacksonException;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 字符串列表反序列化
+ * @author : ginstone
+ * @version : v1.0.0
+ * @since : 2023/4/13 15:12
+ */
+public class ListStringDeserializer extends JsonDeserializer<List<String>> {
+    @Override
+    public List<String> deserialize(JsonParser p, DeserializationContext deserializationContext) throws IOException, JacksonException {
+        return new ArrayList<>(List.of(p.getValueAsString().split(",")));
+    }
+}
