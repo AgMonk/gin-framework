62a56b9bf702c48c23844aa31ddac23f75927d8b
ginstone <hjg719@139.com>
2023-12-22 16:37:46 +0800

    优化

--- spring/src/main/java/com/gin/spring/exception/BusinessException.java
+++ spring/src/main/java/com/gin/spring/exception/BusinessException.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 /**
  * 业务异常
+ *
  * @author : ginstone
  * @version : v1.0.0
  * @since : 2022/12/10 11:05
@@ -20,9 +21,10 @@ public class BusinessException extends RuntimeException {
 
     /**
      * 构造方法
+     *
      * @param httpStatus 状态码
-     * @param title 标题
-     * @param messages 消息
+     * @param title      标题
+     * @param messages   消息
      */
     public BusinessException(HttpStatus httpStatus, String title, List<String> messages) {
         this.httpStatus = httpStatus;
@@ -32,6 +34,14 @@ public class BusinessException extends RuntimeException {
 
     public static BusinessException of(HttpStatus httpStatus, String title, List<String> messages) {
         return new BusinessException(httpStatus, title, messages);
+    }
+
+    public static BusinessException forbidden(String title, String... messages) {
+        return of(HttpStatus.FORBIDDEN, title, messages);
+    }
+
+    public static BusinessException badRequest(String title, String... messages) {
+        return of(HttpStatus.BAD_REQUEST, title, messages);
     }
 
     public static BusinessException of(HttpStatus httpStatus, String title, String... messages) {
